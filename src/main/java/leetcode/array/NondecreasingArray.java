0a9ccb4340b01138a4881ef848990165e5bb39f2
stevenna <theyoung2002@naver.com>
2022-09-26 23:01:25 +0900

    Can't solve alone try again

---
+++ src/main/java/leetcode/array/NondecreasingArray.java
@@ -0,0 +1,23 @@
+package leetcode.array;
+
+import leetcode.graph.MinCosttoConnectAllPoints;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+/**'
+ * 665. Non-decreasing Array
+ * https://leetcode.com/problems/non-decreasing-array/
+ */
+//TODO 이 문제 다시 풀어야함.
+public class NondecreasingArray {
+    @Test
+    void test(){
+
+
+    }
+    public boolean checkPossibility(int[] nums) {
+        return false;
+    }
+}
