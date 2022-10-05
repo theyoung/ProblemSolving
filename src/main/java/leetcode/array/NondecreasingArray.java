f048baa9bc4c8af089484217bc64adc9d0a951a5
stevenna <theyoung2002@naver.com>
2022-10-05 22:09:20 +0900

    String array problem

--- src/main/java/leetcode/array/NondecreasingArray.java
+++ src/main/java/leetcode/array/NondecreasingArray.java
@@ -10,19 +10,65 @@ import java.util.*;
  * 665. Non-decreasing Array
  * https://leetcode.com/problems/non-decreasing-array/
  */
-//TODO 이 문제 다시 풀어야함.
+//TODO 이 문제 다시 풀어야함.(2차)
 public class NondecreasingArray {
     @Test
-    void test(){
-
-        Random random = new Random();
-        System.out.println(random.nextInt(10));
-
-        Map<String, List<Integer>> map = new HashMap<>();
-        map.computeIfAbsent("a", (k)-> new ArrayList<Integer>());
+    void test() {
+        NondecreasingArray nondecreasingArray = new NondecreasingArray();
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{1,2,3}));
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{1,1,1}));
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{1,0,1}));
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{1,0,2}));
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{1,1,-1}));
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{4,2,3}));
+        Assertions.assertFalse(nondecreasingArray.checkPossibility(new int[]{1,0,-1}));
+        Assertions.assertFalse(nondecreasingArray.checkPossibility(new int[]{4,2,1}));
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{1,2,3,100,3,4}));
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{1,2,3,100,3,3}));
+        Assertions.assertFalse(nondecreasingArray.checkPossibility(new int[]{1,2,3,100,3,2}));
+        Assertions.assertFalse(nondecreasingArray.checkPossibility(new int[]{1,2,3,100,3,3,2}));
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{1,2,3,100,4,200,300}));
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{1,2,3,5,4,90,91}));
+        Assertions.assertFalse(nondecreasingArray.checkPossibility(new int[]{3,4,2,3}));
+        Assertions.assertTrue(nondecreasingArray.checkPossibility(new int[]{5,7,1,8}));
 
     }
+
+    //최적화 후
     public boolean checkPossibility(int[] nums) {
-        return false;
+        int count = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i-1] > nums[i]){
+                if(i == 1 || nums[i-2] <= nums[i]){
+                    nums[i-1] = nums[i];
+                } else {
+                    nums[i] = nums[i-1];
+                }
+                count++;
+            }
+        }
+
+        return count < 2;
+    }
+
+    //최적화 전
+    public boolean checkPossibility1(int[] nums) {
+        int count = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i-1] > nums[i]){
+                if(i == 1 || nums.length <= i+1){
+                    nums[i-1] = nums[i];
+                } else if(nums[i-2] <= nums[i]){
+                    nums[i-1] = nums[i-2];
+                } else {
+                    nums[i] = nums[i-1];
+                }
+                count++;
+            }
+        }
+
+        return count < 2;
     }
 }
